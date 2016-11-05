1d2b5d70dfdc1c2e696375f4fb3eea5023b40d2e
prehistoric-penguin <invalid_ms_user@live.com>
2016-11-05 20:26:44 +0800

    fix thrift test client, wait for all response callback finishing

--- test/test_thrift.cpp
+++ test/test_thrift.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include <random>
 #include <stdexcept>
 
@@ -22,53 +23,94 @@ enum operation_t {
   OPERATION_NUM
 };
 
-size_t gRequestSentNum = 0;
-size_t gResponseRecvNum = 0;
-size_t gRequestErrNum = 0;
+std::atomic<size_t> gRequestSentNum{0};
+std::atomic<size_t> gResponseRecvNum{0};
+std::atomic<size_t> gRequestErrNum{0};
 
 class PingCallback : public RequestCallback {
 public:
-  void requestSent() override { ++gRequestSentNum; }
+  void requestSent() override {
+    gRequestSentNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void replyReceived(ClientReceiveState &&) override { ++gResponseRecvNum; }
+  void replyReceived(ClientReceiveState &&) override {
+    gResponseRecvNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void requestError(ClientReceiveState &&) override { ++gRequestErrNum; }
+  void requestError(ClientReceiveState &&) override {
+    gRequestErrNum.fetch_add(1, std::memory_order_release);
+  }
 };
 
 class AddCallback : public RequestCallback {
 public:
-  void requestSent() override { ++gRequestSentNum; }
+  AddCallback(const calculator::Work &w,
+              calculator::CalculatorServiceAsyncClient &client)
+      : work_(w), client_(client) {}
 
-  void replyReceived(ClientReceiveState &&) override { ++gResponseRecvNum; }
+  void requestSent() override {
+    gRequestSentNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void requestError(ClientReceiveState &&) override { ++gRequestErrNum; }
+  void replyReceived(ClientReceiveState &&state) override {
+    gResponseRecvNum.fetch_add(1, std::memory_order_release);
+
+    int calc_res = client_.recv_calculate(state);
+    assert(calc_res == work_.num1 + work_.num2);
+  }
+
+  void requestError(ClientReceiveState &&) override {
+    gRequestErrNum.fetch_add(1, std::memory_order_release);
+  }
+
+private:
+  calculator::Work work_;
+  calculator::CalculatorServiceAsyncClient &client_;
 };
 
 class SubtractCallback : public RequestCallback {
 public:
-  void requestSent() override { ++gRequestSentNum; }
+  void requestSent() override {
+    gRequestSentNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void replyReceived(ClientReceiveState &&) override { ++gResponseRecvNum; }
+  void replyReceived(ClientReceiveState &&) override {
+    gResponseRecvNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void requestError(ClientReceiveState &&) override { ++gRequestErrNum; }
+  void requestError(ClientReceiveState &&) override {
+    gRequestErrNum.fetch_add(1, std::memory_order_release);
+  }
 };
 
 class MultiplyCallback : public RequestCallback {
 public:
-  void requestSent() override { ++gRequestSentNum; }
+  void requestSent() override {
+    gRequestSentNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void replyReceived(ClientReceiveState &&) override { ++gResponseRecvNum; }
+  void replyReceived(ClientReceiveState &&) override {
+    gResponseRecvNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void requestError(ClientReceiveState &&) override { ++gRequestErrNum; }
+  void requestError(ClientReceiveState &&) override {
+    gRequestErrNum.fetch_add(1, std::memory_order_release);
+  }
 };
 
-class DiviceCallback : public RequestCallback {
+class DivideCallback : public RequestCallback {
 public:
-  void requestSent() override { ++gRequestSentNum; }
+  void requestSent() override {
+    gRequestSentNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void replyReceived(ClientReceiveState &&) override { ++gResponseRecvNum; }
+  void replyReceived(ClientReceiveState &&) override {
+    gResponseRecvNum.fetch_add(1, std::memory_order_release);
+  }
 
-  void requestError(ClientReceiveState &&) override { ++gRequestErrNum; }
+  void requestError(ClientReceiveState &&) override {
+    gRequestErrNum.fetch_add(1, std::memory_order_release);
+  }
 };
 
 random_t gRng(5);
@@ -84,7 +126,7 @@ void random_request(calculator::CalculatorServiceAsyncClient &client, int id) {
     break;
   case ADD:
     w.op = calculator::Operation::ADD;
-    client.calculate(folly::make_unique<AddCallback>(), id, w);
+    client.calculate(folly::make_unique<AddCallback>(w, client), id, w);
     break;
   case SUBTRACT:
     w.op = calculator::Operation::SUBTRACT;
@@ -96,7 +138,7 @@ void random_request(calculator::CalculatorServiceAsyncClient &client, int id) {
     break;
   case DIVIDE:
     w.op = calculator::Operation::DIVIDE;
-    client.calculate(folly::make_unique<DiviceCallback>(), id, w);
+    client.calculate(folly::make_unique<DivideCallback>(), id, w);
     break;
   default:
     LOG(FATAL) << "unreachable";
@@ -108,19 +150,23 @@ int main(int argc, char *argv[]) {
                           " [OPTIONS]...");
   google::ParseCommandLineFlags(&argc, &argv, true);
 
+  FLAGS_log_dir = ".";
+
+  google::InitGoogleLogging(argv[0]);
+
   folly::EventBase eb;
   calculator::CalculatorServiceAsyncClient client(
       HeaderClientChannel::newChannel(async::TAsyncSocket::newSocket(
           &eb, {FLAGS_srv_host, static_cast<uint16_t>(FLAGS_srv_port)})));
 
-  constexpr int kMax = 1e8;
+  constexpr int kMax = 1e6;
 
   std::thread thr([&]() { eb.loopForever(); });
   thr.detach();
 
-  for (int i = 0; i < kMax; ++i) {
+  for (int i = 1; i < kMax; ++i) {
     try {
-      eb.runInEventBaseThread([&](){  random_request(client, i); });
+      eb.runInEventBaseThread([&, i]() { random_request(client, i); });
     } catch (TException &ex) {
       std::cout << "  Thrift error: " << ex.what() << std::endl;
     } catch (std::exception &ex) {
@@ -131,11 +177,18 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   eb.runInEventBaseThreadAndWait([&]() { eb.terminateLoopSoon(); });
 
-  std::cout << "sent :" << gRequestSentNum
-      << ", resp :" << gResponseRecvNum
-      << ", err  :" << gRequestErrNum << std::endl;
+  std::cout << "sent :" << gRequestSentNum.load(std::memory_order_relaxed)
+            << ", resp :" << gResponseRecvNum.load(std::memory_order_relaxed)
+            << ", err  :" << gRequestErrNum.load(std::memory_order_relaxed)
+            << std::endl;
+
+  if (gRequestSentNum.load(std::memory_order_relaxed) ==
+      gResponseRecvNum.load(std::memory_order_relaxed)) {
+    std::cout << "TEST PASSED" << std::endl;
+  }
 
   return 0;
 }
