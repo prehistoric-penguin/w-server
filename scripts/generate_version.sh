844f5b1f87b161f9c402074a6bb0d41bb9839e2e
风 <invalid_ms_user@live.com>
2020-03-04 13:59:15 +0800

    Update generate_version.sh

--- scripts/generate_version.sh
+++ scripts/generate_version.sh
@@ -7,15 +7,18 @@ DATE=`git log -1 --decorate=short | grep Date | head -1`
 MESSAGE=`git log -1 --decorate=short | tail -1`
 GENERATE_DATE=`date -R`
 
-echo "// An auto generated file,do not modify it." > $target_file
-echo "// @Generate date:"$GENERATE_DATE >> $target_file
-echo "" >> $target_file
-echo "#include \"version.h\"" >> $target_file
-echo "" >> $target_file
-echo "namespace w {" >> $target_file
-echo "std::string VersionMessage::author_ = \"$AUTHOR\";" >> $target_file
-echo "std::string VersionMessage::commit_hash_ = \"$COMMIT\";" >> $target_file
-echo "std::string VersionMessage::date_ = \"$DATE\";" >> $target_file
-echo "std::string VersionMessage::commit_message_ = \"$MESSAGE\";" >> $target_file
-echo "} // namespace w" >> $target_file
+cat > $target_file <<- EOM
+// WARN: Auto generate file, pleaes don't modify it
+// @Generate data: $GENERATE_DATE
+
+"#include version.h" 
+
+namespace w{
+std::string VersionMessage::author_ = "$AUTHOR";
+std::string VersionMessage::commit_hash_ = "$COMMIT"; 
+std::string VersionMessage::date_ = "$DATE";
+std::string VersionMessage::commit_message_ = "$MESSAGE";
+} // namespace w
+
+EOM
 exit 0
